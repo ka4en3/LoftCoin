65c1f8e475cf6c1f2de670a30990654fcab83bef
FK <ka4en3@gmail.com>
2022-06-02 15:17:51 +0400

    howework_1 commit. homework_1 was done already in init commit. add comments here only.

--- app/src/main/java/com/akchimwf/loftcoin/ui/welcome/WelcomeActivity.java
+++ app/src/main/java/com/akchimwf/loftcoin/ui/welcome/WelcomeActivity.java
@@ -13,34 +13,50 @@ import com.akchimwf.loftcoin.ui.main.MainActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    /*flag to show or not WelcomeActivity */
     public static final String KEY_SHOW_WELCOME = "show_welcome";
 
     private ActivityWelcomeBinding binding;
+    /*PagerSnapHelper helps to show RecycleView as ViewPager, we don't use ViewPager here*/
     private PagerSnapHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /*ActivityWelcomeBinding class comes from 'viewBinding' at build.grade*/
         binding = ActivityWelcomeBinding.inflate(getLayoutInflater());
+
+        /*setContentView needs View -> use getRoot()*/
         setContentView(binding.getRoot());
+
+        /*set LayoutManager and adapter to RecycleView*/
         binding.recycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
         binding.recycler.setAdapter(new WelcomeAdapter());
+
+        /*define Helper*/
         helper = new PagerSnapHelper();
         helper.attachToRecyclerView(binding.recycler);
+
+        /*set OnClickListener to btnStart, set KEY_SHOW_WELCOME flag to False */
         binding.btnStart.setOnClickListener(v -> {
             PreferenceManager.getDefaultSharedPreferences(this)
                     .edit()
                     .putBoolean(KEY_SHOW_WELCOME, false)
                     .apply();
+            /*start MainActivity*/
             startActivity(new Intent(this, MainActivity.class));
+            /*needs to close WelcomeActivity here as we proceed with MainActivity further*/
             finish();
         });
     }
 
     @Override
     protected void onDestroy() {
+        /*onDestroy de-attach adapter and helper */
         helper.attachToRecyclerView(null);
         binding.recycler.setAdapter(null);
+
         super.onDestroy();
     }
 }
